9ebae8b928c1379fe5c9b3bb566c763ae2db0cdf
JAVA-DSA <ronbonnke789@gmail.com>
2024-04-11 15:17:24 +0530

    your message

---
+++ corebasics/index.js
@@ -0,0 +1,26 @@
+var x = 7;
+
+function getName() {
+  console.log("Welcome to JavaScript");
+}
+
+getName();
+console.log(x);
+console.log(getName);
+
+// functions
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a() {
+  var x = 10;
+  console.log(x);
+}
+
+function b() {
+  var x = 100;
+  console.log(x);
+}
--- corebasics/squares.js
+++ corebasics/squares.js
@@ -5,8 +5,13 @@ function square(num) {
   return ans;
 }
 
+// passing n as an argument here down below
 var square2 = square(n);
+
+// passing 4 in the arguments directly here down below
 var square3 = square(4);
 
 console.log("Square of", n, "is", square2);
+
+//  a brand new execution context will  be created here when the function call is made
 console.log("Square of 4 is", square3);
