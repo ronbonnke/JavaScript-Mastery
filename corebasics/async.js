05748c339bb53f6aac6e601419de3a418440a2bb
JAVA-DSA <ronbonnke789@gmail.com>
2024-04-12 17:26:43 +0530

    js

--- corebasics/async.js
+++ corebasics/async.js
@@ -1,18 +1,40 @@
 const func = () => {
   const randomNumber = Math.random(10);
-  if (randomNumber > 0.5) {
-    throw new Error("It is more");
-  } else {
-    console.log(randomNumber);
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (randomNumber < 0.5) {
+        resolve(randomNumber);
+      } else {
+        reject("the number is more");
+      }
+    }, 6000);
+  });
 };
 
 const name = async () => {
   try {
-    await func();
-    console.log("It is less");
+    const response = await fetch("https://dummyjson.com/products/1");
+    const result = await response.json();
+    // console.log(result.id);
+    return result;
   } catch (error) {
     console.log(error);
   }
 };
-name();
+const data = async () => {
+  try {
+    const data1 = await name();
+    if (data1) {
+      data1.images.map((item) => {
+        console.log(item);
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+data();
+
+// if(data?:
+// <></>:
+// <>{result.name}</>)
