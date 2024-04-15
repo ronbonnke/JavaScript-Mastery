5c69e27652b517867e20ffbbdff36c098ad21a7a
JAVA-DSA <ronbonnke789@gmail.com>
2024-04-15 13:30:00 +0530

    your message

--- corebasics/let-const.js
+++ corebasics/let-const.js
@@ -1,6 +1,31 @@
-console.log(b); // gives undefined
+var c;
+c = 99; // can reassign value to var types
+console.log(c);
 
-// console.log(a); // gives error as a is not defined
+let a;
+a = 10; // can reassign value to const types
+console.log(a);
 
-let a = 10;
-var b = 100;
+const b = 100;
+// b = 1200; // CANNOT reassign value to const types
+console.log(a);
+console.log(b);
+
+// 1. var is function scoped
+// 2. let and const are block scope
+// 1. var is hoisted
+// 2. let and const are not hoisted
+
+// 3. var can be re-declared
+// 4. let and const cannot be re-declared
+
+// {
+// eg :
+// var a = 10;
+// var a = 20;
+// console.log(a);
+// this cannot be done
+// }
+
+// 5. var can be updated
+// 6. let and const cannot be updated
