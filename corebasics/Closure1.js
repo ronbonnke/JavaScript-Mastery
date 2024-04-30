50fadc9783052c07d343e6d3e546f91f1e2a4cb0
JAVA-DSA <ronbonnke789@gmail.com>
2024-04-30 20:59:48 +0530

    your message

--- corebasics/Closure1.js
+++ corebasics/Closure1.js
@@ -2,5 +2,7 @@ function x() {
   var i = 1;
   setTimeout(function () {
     console.log(i);
-  }, 1000);
+  }, 3000); // waits 3 mins
+  console.log("Hello JavaScript"); // does'nt wait for anyone
 }
+x();
