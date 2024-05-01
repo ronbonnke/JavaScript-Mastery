97c0456bc6cd92ca493a17f2907a61768df001b1
JAVA-DSA <ronbonnke789@gmail.com>
2024-05-02 00:33:48 +0530

    your message

--- corebasics/filter.js
+++ corebasics/filter.js
@@ -22,3 +22,27 @@ names.filter((item) => {
 // });
 
 // console.log(namesStartingWithA); // Output: ['Alice', 'Anna', 'Amy']
+
+//example 1:
+const students = [
+  { name: "Alice", age: 22 },
+  { name: "Bob", age: 19 },
+  { name: "Charlie", age: 25 },
+  { name: "David", age: 18 },
+];
+
+const olderStudents = students.filter((student) => {
+  return student.age > 20;
+});
+
+console.log(olderStudents);
+// Output: [{ name: 'Alice', age: 22 }, { name: 'Charlie', age: 25 }]
+
+//
+//
+// example 2:
+const values = [10, 0, "", "Hello", false, true, undefined, null, NaN];
+const truthyValues = values.filter((value) => {
+  return Boolean(value);
+});
+console.log(truthyValues); // Output: [10, 'Hello', true]
