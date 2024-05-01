ba86eb0ac7d73c7b7f297171c1f25a280c65a631
JAVA-DSA <ronbonnke789@gmail.com>
2024-05-01 16:29:03 +0530

    your message

--- corebasics/promises.js
+++ corebasics/promises.js
@@ -1,1 +1,13 @@
 // Promises in js
+
+const cart = ["shoe", "pant", "shirt"];
+
+createOrder(cart, function (orderId) {
+  proceed(orderId);
+});
+
+const Promise = createOrder(cart);
+
+Promise.then(function (orderId) {
+  proceed(orderId);
+});
