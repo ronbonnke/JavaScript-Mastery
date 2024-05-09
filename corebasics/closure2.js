6d898d6b4688a26ce6fa4c82183ea5fa2a5b46ff
JAVA-DSA <ronbonnke789@gmail.com>
2024-05-09 17:50:41 +0530

    your message

---
+++ corebasics/closure2.js
@@ -0,0 +1,13 @@
+// Javascript does not wait for anyone
+function x() {
+  for (let i = 1; i <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
+  console.log("Hello Js"); // print first be'coz it won't for others
+}
+x();
+
+// if we use var we get 66666 because of closure
+// if we use let/const we get 12345
