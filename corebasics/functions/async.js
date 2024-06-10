a42420a00d61797b114847db611bfc619c3ab776
JAVA-DSA <ronbonnke789@gmail.com>
2024-06-10 09:22:22 +0530

    your message

--- corebasics/functions/async.js
+++ corebasics/functions/async.js
@@ -1,34 +1,18 @@
-const func = () => {
-  const randomNumber = Math.random(10);
+const fun = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (randomNumber < 0.5) {
-        resolve(randomNumber);
-      } else {
-        reject("the number is more");
-      }
-    }, 6000);
+    const a = 20;
+    if (a === 10) {
+      resolve(12);
+    } else {
+      reject(-1);
+    }
   });
 };
 
-const name = async () => {
-  try {
-    const response = await fetch("https://dummyjson.com/products/1");
-    const result = await response.json();
-    // console.log(result.id);
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-};
 const data = async () => {
   try {
-    const data1 = await name();
-    if (data1) {
-      data1.images.map((item) => {
-        console.log(item);
-      });
-    }
+    const result = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    console.log(result.json()); // api data
   } catch (error) {
     console.log(error);
   }
