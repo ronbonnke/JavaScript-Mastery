891e21d336ee887910968078fdad054be3232dd9
JAVA-DSA <ronbonnke789@gmail.com>
2024-06-17 18:37:39 +0530

    your message

--- corebasics/basic.js
+++ corebasics/basic.js
@@ -1,32 +1,7 @@
-// all types of datatypes can be used inside a var in js
-
-var a;
-console.log(a);
-
-a = 10;
-console.log(a);
-
-a = "Hello";
-console.log(a);
-
-a = true;
-console.log(a);
-
-a = null;
-console.log(a);
-
-// never ever do this in js
-// not a good practice
-// a = undefined;
-// console.log(a);
-
-// Asked in Accenture interview
-// give me the output
-console.log(1);
-setTimeout(function () {
-  console.log(2);
-}, 5000);
-setTimeout(function () {
-  console.log(3);
-}, 0);
-console.log(4);
+function addEventListener() {
+  let count = 0;
+  document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("button clicked ", count++);
+  });
+}
+addEventListener();
